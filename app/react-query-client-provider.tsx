65f0434479bcd097f36d00c46ec8d3ae3d9f4a60
doong-jo <sdong001@gmail.com>
2025-12-28 21:49:04 +0900

    fix: react-query default option 제거

--- app/react-query-client-provider.tsx
+++ app/react-query-client-provider.tsx
@@ -3,13 +3,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function makeQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 60 * 1000,
-      },
-    },
-  });
+  return new QueryClient();
 }
 
 let browserQueryClient: QueryClient | undefined = undefined;
